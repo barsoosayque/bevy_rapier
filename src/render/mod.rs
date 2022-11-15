0340f5ef61b534803da56c6215ec3b8159b4c336
barsoosayque <shtoshich@gmail.com>
2022-11-15 18:24:42 +0700

    fix RapierDebugRenderPlugin::enabled not being used

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -111,7 +111,7 @@ impl Plugin for RapierDebugRenderPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(lines::DebugLinesPlugin::always_on_top(self.always_on_top))
             .insert_resource(DebugRenderContext {
-                enabled: true,
+                enabled: self.enabled,
                 global: self.global,
                 pipeline: DebugRenderPipeline::new(self.style, self.mode),
                 always_on_top: self.always_on_top,
